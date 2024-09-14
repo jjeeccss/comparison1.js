84dbb7d8ef1d6c6b25869386b01d60cf51d93e60
jjeeccss <jeicogacusna24@gmail.com>
2024-09-14 15:56:19 +0800

    Inital commit

---
+++ comparison.js
@@ -0,0 +1,35 @@
+let firstFavNum = 27;
+let secondFavNum= 08;
+
+//greater than
+console.log(firstFavNum > secondFavNum);
+
+//less than
+console.log(firstFavNum < secondFavNum); 
+
+//greater than or equal to
+console.log(firstFavNum >= secondFavNum); 
+
+//less than to equal to
+console.log(firstFavNum <= secondFavNum); 
+
+//strict equality (===)
+console.log(firstFavNum === secondFavNum); 
+
+// loose equality  (==)
+console.log(firstFavNum == secondFavNum);
+
+//strict inequality (!==)
+console.log(firstFavNum !== secondFavNum);
+
+//loose inequality (!=)
+console.log(firstFavNum != secondFavNum);
+
+//AND operator
+console.log(firstFavNum > 27 && secondFavNum < 08);
+
+//OR operator
+console.log(firstFavNum < 27 || secondFavNum > 08);
+
+//NOT operator
+console.log(!(firstFavNum > secondFavNum))
